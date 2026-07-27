cdbb360abdba0bb5c5014dd545bfb5db89821c81
Miralia <artiste_whipper6a@icloud.com>
2026-07-28 00:25:22 +0800

    chore(deps): update yacomp-web to 5.9.5-web.5

--- src/lib/config/yacomp-web.ts
+++ src/lib/config/yacomp-web.ts
@@ -1,3 +1,3 @@
-export const YACOMP_WEB_VERSION = "5.9.5-web.4";
-export const YACOMP_WEB_SHA256 = "59a782b4d667e990dc2eed4ea510160f3becb20aee6815ff1746fa8098390353";
+export const YACOMP_WEB_VERSION = "5.9.5-web.5";
+export const YACOMP_WEB_SHA256 = "9c222c89f0fda923124cc0c290e23d93f75e43d4f1a221a3f90fbda49d20c770";
 export const YACOMP_WEB_ASSET_URL = `/vendor/yacomp-web/v${YACOMP_WEB_VERSION}/yacomp-web.esm.js`;
